817912332637463003046c9e25c978f499f17dd3
Sun flower <MLIU2@LTU.EDU>
2025-11-07 11:29:23 -0500

    add logs utility

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const handleBlogRouter = require('./src/router/blog');
 const handlerUserRouter = require('./src/router/user');
 const querystring = require('querystring');
 const { get, set } = require('./src/db/myRedis');
+const { access } = require('./src/utils/log');
 
 // 获取cookie的过期时间
 const getCookieExpires = () => {
@@ -51,6 +52,13 @@ const getPostData = (req) => {
 };
 
 const serverHandler = (req, res) => {
+  // 记录 access 日志 (使用自己写的log模块)
+  access(
+    `${req.method} -- ${req.url} -- ${
+      req.headers['user-agent']
+    } -- ${Date.now()}`
+  );
+
   // 设置返回格式为 JSON
   res.setHeader('Content-Type', 'application/json');
 
---
+++ src/utils/log.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+// 生成 Write Stream 对象
+function createWriteStream(fileName) {
+  const fullFileName = path.join(__dirname, '../../logs', fileName);
+  // 使用流式写入, 写入文件的stream对象
+  const writeStream = fs.createWriteStream(fullFileName, { flags: 'a' }); // 'a' 代表 append，追加写入
+  return writeStream;
+}
+
+// 写日志, 往哪里写？-> 写入流对象writeStream; 写什么？-> 日志内容log
+function writeLog(writeStream, log) {
+  writeStream.write(log + '\n'); //通过.write方法写入内容
+}
+
+// 写access访问日志
+const accessWriteStream = createWriteStream('access.log');
+function access(logContent) {
+  // 写日志, 往哪里写？-> 写入流对象writeStream; 写什么？-> 日志内容logContent
+  writeLog(accessWriteStream, logContent);
+}
+
+module.exports = {
+  access,
+};
